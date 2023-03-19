c6d1f49e86bf11d987d248ec1c6aafc12cfc670a
Steph974_ <dimitry97490@gmail.com>
2023-03-19 18:19:24 +0400

    Uniformisation

---
+++ deletePost.php
@@ -0,0 +1,63 @@
+<!-- 
+⣿⣿⣿⡇⠄⣠⣬⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⡝⠙⣿⣷⡄⣹⣿⣿⣿⣿⠛⠛⠛⠛⠋⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠟⠿⡿⠿⠿⠿⠿⠿⠿⠿⢛⡻⡿⠿⣿⣿⣿⣿⣿⣿⣿
+⣄⠄⡼⣿⣷⣿⣿⣿⡉⣿⣿⣿⣿⣿⡟⢿⣿⠄⠄⠄⠄⠘⣿⣿⣿⣿⡟⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⣿⣿⣿⣿⣿⣿⣿
+⠄⢱⡓⠉⠙⠙⠛⣻⠃⣿⣿⣿⣿⣿⠇⠸⣿⠄⠄⠄⠄⠄⠸⣿⡻⣿⣿⡘⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⡟⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⣿⣿⠟⡿⠿⣿⣿
+⡠⠞⢯⣤⣠⣤⣾⣷⠄⣿⣿⣿⣿⣿⠄⠄⢻⠄⠄⠄⠄⠄⠄⠘⢧⠙⠻⣿⣄⠻⣿⣿⣿⣿⡟⣿⣿⣿⣿⠈⢿⣧⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⣷⠋⡾⣇⣀⣸⣿
+⣿⡿⣿⣿⢏⡿⠉⠉⠄⣿⣿⣿⣿⣿⠄⠄⠘⠒⠒⠒⠒⠒⠶⠶⠤⠄⠄⠈⠙⠂⠈⠙⠛⠋⠉⠉⠭⠿⠿⠶⠾⠛⠚⠛⠛⠛⠋⣿⣿⣿⣿⣿⡇⣿⣿⣿⣿⣿⣿⣿
+⠿⡀⠄⠘⠉⠇⠂⠄⠄⣇⢻⣿⣿⣯⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣿⣿⣿⣿⣿⡇⣿⣿⣿⣿⣿⣿⣿
+⡆⠁⠄⠊⠙⠒⢄⣀⠄⣿⠐⢿⣿⣿⢠⣶⣦⣤⣀⣀⣀⣀⣀⣀⣀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢀⣀⣀⣀⠄⢀⣀⣀⣤⣤⣤⣿⣿⡯⢿⣿⡇⣿⣿⣿⣿⣿⣿⣿
+⠄⠄⠄⣀⡀⠄⢈⣿⡆⣻⡇⠄⠛⣿⡆⠄⠉⠉⠙⠛⠛⠋⠉⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠈⠉⠙⠛⠛⠛⠋⠉⠒⢰⣿⣿⡇⠘⣿⡇⣿⣿⣿⣿⣿⣿⣿
+⣀⢠⠄⠁⠲⣻⣿⠿⠃⣿⣿⡀⠄⠘⢿⡀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢸⣿⣿⢀⣴⣿⡇⣿⣿⣿⣿⣿⣿⣿
+⣿⣷⣀⠄⣀⢟⡩⣲⠄⣿⣿⣿⣤⣀⠈⠻⣄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣼⣿⣿⣿⣿⣿⡇⣹⣿⣿⣿⣿⣿⣿
+⣵⣝⢿⣟⡯⠭⡹⣿⣄⣿⣿⣿⣿⣿⣷⡀⠈⠳⢄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢀⣿⣿⣿⣿⣿⣿⡇⢮⣭⡯⠍⠹⣻⣿
+⣿⣿⣿⣿⡛⣫⣴⣿⠌⢹⢿⣿⣿⣿⣿⣿⡄⠄⠄⠈⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢀⣾⣿⣿⣿⣿⣿⣿⡇⣚⠗⠄⠄⣰⣿⣿
+⡟⠏⠉⠉⠻⣦⡉⢩⠤⠈⠉⠄⣩⡿⠛⠛⠛⠂⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠛⠛⠛⠛⠛⣛⣛⣛⣃⣿⣷⣶⣿⣿⣿⣿
+⣩⠅⠄⠄⢺⠿⠛⢦⣇⢠⠈⣨⣾⣷⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⣥⣤
+⣣⣴⣇⢸⠟⣄⣀⣸⣿⣿⣿⣿⣿⣯⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠺⣿⣦⣄⣀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢠⣶⣶⢿⣿⣿⣿⣟⣿⢿⣿⣿⣿⣿⣿⣿ 
+-->
+<?php
+// démarrer une session PHP
+session_start();
+
+// inclure le fichier de connexion à la base de données
+include 'lib/dbConnect.php';
+
+// établir la connexion à la base de données
+$bd = new DbConnect();
+$conn = $bd->connect();
+
+// vérifier si l'ID de l'article à supprimer est présent dans l'URL
+if (isset($_GET['idArticle']) && !empty($_GET['idArticle'])) {
+    // récupérer l'ID de l'article à supprimer
+    $idArticle = $_GET['idArticle'];
+
+  
+
+   
+    // préparer la requête SQL pour supprimer l'article avec l'ID donné
+    $sql = "DELETE FROM likes WHERE idArticle = :article_id; DELETE FROM commentaires WHERE idArticle = :article_id; DELETE FROM article WHERE id = :article_id";
+
+    // préparer la requête pour l'exécution
+    $req = $conn->prepare($sql);
+
+    // lier les paramètres de la requête
+    $req->bindParam(':article_id', $idArticle);
+
+    // exécuter la requête
+    if ($req->execute()) {
+        // redirection vers la page d'accueil si la suppression a réussi
+        header("Location: index.php");
+        exit();
+        // Sinon
+    } else {
+        // redirection vers la page de l'article en cas d'erreur
+        header("Location: viewArticles.php?idArticle=$idArticle&error=delete");
+        exit();
+    }
+} else {
+    // redirection vers la page d'accueil si l'ID de l'article est manquant dans l'URL
+    header("Location: index.php");
+    exit();
+}
+?>
--- updatePost.php
+++ updatePost.php
@@ -16,98 +16,96 @@
 ⣩⠅⠄⠄⢺⠿⠛⢦⣇⢠⠈⣨⣾⣷⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⣥⣤
 ⣣⣴⣇⢸⠟⣄⣀⣸⣿⣿⣿⣿⣿⣯⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠺⣿⣦⣄⣀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢠⣶⣶⢿⣿⣿⣿⣟⣿⢿⣿⣿⣿⣿⣿⣿ 
 -->
+
 <?php
 session_start();
 include 'lib/dbConnect.php';
 $bd = new DbConnect();
 $conn = $bd->connect();
 
-$idArt = $_GET['idArticle'];
+$idArticle = $_GET['idArticle'];
 
-$sql = "SELECT * FROM article WHERE id= :id";
+$sql = "SELECT * FROM article WHERE id = :id";
 $req = $conn->prepare($sql);
-$req->bindParam(':id', $idArt);
+$req->bindParam(':id', $idArticle);
 $req->execute();
 $art = $req->fetch(PDO::FETCH_ASSOC);
 ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="stylesheet" href="styles/articlesView.css">
-    <link rel="stylesheet" href="styles/main.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $art['titre'] ?></title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <link rel="stylesheet" href="styles/main-center.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Accueil</title>
+  <script src="js/tinymce/tinymce.min.js"></script>
 </head>
 
 <body>
-    <div class="background">
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
 
-        <?php require_once "lib/navbar.php" ?>
-        <div class="containerArticle">
-            <div>
-                <h1 class="titreArticle"><?php echo $art['titre'] ?></h1>
-                <img class="viewArticle" src="images/<?php echo $art['image'] ?>" alt="Image" class="image">
-                <p class="textArticle"><?php echo $art['datePublication'] ?></p>
-                <?php
 
-                if(!isset($_SESSION['User'])){
-                    echo " <div class='likes'><a  href='login-form.php'><img height='15px' src='https://cdn-icons-png.flaticon.com/512/1077/1077035.png'><p>  " . $art['likeTotal'] . "</p></a> </div>";
-                }else{
+  <?php require_once "lib/navbar.php" ?>
 
-                $sql = "SELECT idUser FROM likes WHERE idArticle = :idArt";
-                // Pour éviter les injections SQL
-                $result = $conn->prepare($sql);
-                $result->bindParam(':idArt', $idArt);
-                $result->execute();
-                $users = $result->fetchAll(PDO::FETCH_ASSOC);
-                $like = FALSE;
-                foreach ($users as $user) {
-                    if ($user['idUser'] == $_SESSION["User"]) {
-                        $like = 'TRUE';
-                    }
-                }
-                if ($like == 'TRUE') {
-                    echo " <div class='likes'> <a href='like.php?like=TRUE&idUser=" . $_SESSION["User"] . "&idArt=" . $idArt . "'><img height='15px' src='https://cdn-icons-png.flaticon.com/512/2107/2107845.png'><p>  " . $art['likeTotal'] . "</p></a> </div>";
-                } else {
-                    echo " <div class='likes'><a  href='like.php?idUser=" . $_SESSION["User"] . "&idArt=" . $idArt . "'><img height='15px' src='https://cdn-icons-png.flaticon.com/512/1077/1077035.png'><p>  " . $art['likeTotal'] . "</p></a> </div>";
-                }
-            }
+  <div class="container">
+    <form class="fomulaire" action="scriptUpdate.php" method="post" enctype="multipart/form-data">
+      <input style="display: none;" value="<?php echo $art['id'] ?>" name="idArt" type="text">
+      <div>
+        <label for="">Titre de l'article</label> <br>
+        <input value="<?php echo $art['titre'] ?>" type="text" name="titre" required>
+      </div>
+      <div>
+        <label for="">Image de l'article</label> <br>
+        <input src="images/<?php echo $art['image'] ?>" type="file" name="image" required>
+      </div>
+      <div>
+        <label for="">Catégorie de l'article</label> <br>
 
-                ?>
-                <?php echo $art['contenu'] ?>
-                <?php if(isset($_SESSION['Modo'])){
-            echo '<a href="updatePost.php?idArticle=' . $art['id'] . '">Modifier l\'article</a> ';
-            echo '<a href="deletePost.php?idArticle=' . $art['id'] . '">Supprimer l\'article</a>';
+        <?php
+        $conn = $bd->connect();
+        $sql = "SELECT id,libelle FROM categorie";
+        $req = $conn->prepare($sql);
+        $req->execute();
+        ?>
+        <select name="cate" id="" required>
+          <?php foreach ($req as $cate) {  ?>
 
-        } ?>
-            </div>
-        </div>
-    </div>
+            <option value="<?= $cate['id'] ?>"> <?= $cate['libelle'] ?></option>
+
+          <?php     }  ?>
+        </select>
+      </div>
+      <div>
+        <label for="">Contenu de l'article</label>
+        <textarea name="article" cols="30" rows="10"> <?php echo $art['contenu'] ?></textarea>
+      </div>
+      <button name="update" type="submit">Modifier</button>
+    </form>
+  </div>
+
+  <script type="text/javascript" language="javascript">
+
+
+    tinymce.init({
+      selector: "textarea",
+      width: 900,
+      height: 670,
+      menubar: false,
+      statusbar: false,
+      extended_valid_elements : "iframe[src|frameborder|style|scrolling|class|width|height|name|align]",
+      plugins: 'media',
+  menubar: 'insert',
+  media_live_embeds: true
+
+    });
+    
+    
+  </script>
 </body>
 
 </html>
+
+
