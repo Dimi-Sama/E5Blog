84cd69108413caca82b50a19ce8d2c08d3bca105
Steph <dimitry97490+git@gmail.com>
2023-05-09 18:35:16 +0400

    Version 0.5.5

--- lib/dbConnect.php
+++ lib/dbConnect.php
@@ -4,9 +4,9 @@
 	*/
 	class DbConnect {
 		private $server = 'localhost';
-		private $dbname = 'e5_blog';
-		private $user = 'user_blog';
-		private $pass = 'M0mmy_K@fk@';
+		private $dbname = 'blog_e5';
+		private $user = 'root';
+		private $pass = '';
 
 		public function connect() {
 			try {
@@ -18,4 +18,21 @@
 			}
 		}
 	}
+
+	// class DbConnect {
+	// 	private $server = 'localhost';
+	// 	private $dbname = 'e5_blog';
+	// 	private $user = 'user_blog';
+	// 	private $pass = 'M0mmy_K@fk@';
+
+	// 	public function connect() {
+	// 		try {
+	// 			$conn = new PDO('mysql:host=' .$this->server .';dbname=' . $this->dbname, $this->user, $this->pass);
+	// 			$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	// 			return $conn;
+	// 		} catch (\Exception $e) {
+	// 			echo "Database Error: " . $e->getMessage();
+	// 		}
+	// 	}
+	// }
  ?>
